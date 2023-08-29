2815a947e27a30b1b1dd04b42bf5139eb39455cd
Douglas Korinke <doug.korinke@centricsoftware.com>
2023-08-29 11:39:13 -0700

    Final module 2 updates

--- code/module-02/m02-start/module02.ts
+++ code/module-02/m02-start/module02.ts
@@ -79,7 +79,7 @@ console.log(whichMonths(Season.Spring));
 
    let randomNumbers: number[] = [];
    let nextNumber: number;
-   
+    
    for (let i = 0; i < 10; i++) {
        nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
        randomNumbers.push(nextNumber);
